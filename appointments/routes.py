780a58853379c364fbbf0ce38bc2204b036a5662
Spandan Majumder <spandan301298@gmail.com>
2025-04-23 20:49:50 +0530

    added endpoint

--- appointments/routes.py
+++ appointments/routes.py
@@ -6,6 +6,8 @@ from datetime import datetime
 
 appointments_bp = Blueprint('appointments', __name__)
 
+
+#endpoint for booking an appointment
 @appointments_bp.route('/book', methods =["POST"])
 @jwt_required
 def book_appointments():
@@ -15,9 +17,50 @@ def book_appointments():
     doctor = User.query.filter_by(id = data['doctor_id'], role='doctor').first()
     if not doctor:
         return jsonify({'error':"invalid doctor id"})
+    
+
+    #check if doctor already has an appointment at the given date and time
+    existing = Appointment.query.filter_by(
+        doctor_id = data['doctor_id'],
+        date = datetime.strptime(data['date'], '%Y-%m-%d').date(),
+        time = datetime.strptime(data['time'], '%H-%M').time()
+    ).first()
+
+    if existing:
+        return jsonify({'error': 'Doctor already has an appointment at thist time'})
+    
+
+#to view upcoming appointments
+@appointments_bp.route('/upcoming')
+@jwt_required
+def view_upcoming_appointments():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+
+    now = datetime.utcnow()
+    appointments = []
+
+    if user.role == "doctor":
+        appointments = Appointment.query.filter_by(doctor_id = user_id).filter(Appointment.date >= now.date()).all
+    elif user.role == 'patient':
+        appointments = Appointment.query.filter_by(patient_id = user_id).filter(Appointment.date >= now.date()).all
+    else:
+        return jsonify({"error": 'invalid user role'}), 403
+    
+    return jsonify([{
+        "id": a.id,
+        "date": a.date.isoformat(),
+        "time": a.time.strftime('%H:%M'),
+        "doctor_id": a.doctor_id,
+        "patient_id": a.patient_id,
+        "status": a.status
+    } for a in appointments]), 200
+
+
+
 
     new_appt = Appointment(
-        patient_id = current_user(get_jwt_identity),
+        patient_id = current_user,
         doctor_id = data['doctor_id'],
         date = datetime.strptime(data['date'], '%Y-%m-%d').date(),
         time = datetime.strptime(data['time'], '%H:%M').time()
